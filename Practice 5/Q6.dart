b0ddf2e638bc95d450109f8db9f063fe7a955665
Hami-Mashfiq <hamimahdi2526@gmail.com>
2025-10-23 23:10:58 +0600

    Add file deletion logic in Q6.dart

---
+++ Practice 5/Q6.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  File file = File('hello_copy.txt');
+
+  if(file.existsSync()) {
+    file.deleteSync();
+    print('hello_copy.txt deleted successfully!');
+  }
+  else {
+    print('File does not exist.');
+  }
+}
