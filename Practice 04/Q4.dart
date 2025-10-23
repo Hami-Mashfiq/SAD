94d633c6a19f835f8ebb98b4823e92cdab36e46f
Hami-Mashfiq <hamimahdi2526@gmail.com>
2025-10-23 22:59:47 +0600

    Add Q4.dart to display days of the week

---
+++ Practice 04/Q4.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+void main() {
+    List<String> days= [];
+    days.addAll(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
+    
+    print('DAYS OF THE WEEK :- ${days.join(', ')}');
+}
