508a101669c86cd0f46d039579326e7fe8fc9a0b
Hami-Mashfiq <hamimahdi2526@gmail.com>
2025-10-23 23:08:30 +0600

    Create Q2.dart to append name to hello.txt

---
+++ Practice 5/Q2.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  String Name = 'ZORO TARO';
+  File file = File('hello.txt');
+
+  file.writeAsStringSync('$Name\n', mode: FileMode.append);
+  print('Friend name appended to hello.txt successfully!');
+}
