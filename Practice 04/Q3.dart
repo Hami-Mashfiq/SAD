03540e3ae0a83ea44fadfed84d0cb898a5d79643
Hami-Mashfiq <hamimahdi2526@gmail.com>
2025-10-23 22:59:07 +0600

    Create Q3.dart

---
+++ Practice 04/Q3.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+void main() {
+    int n = int.parse(stdin.readLineSync()!);
+    List<int> p = [];
+    int ans = 0;
+    for(int i = 0;  i< n; i++) {
+        int a = int.parse(stdin.readLineSync()!);
+        p.add(a);
+    }
+    for (int i in p) {
+        ans += i;
+    }
+    print(ans);
+}
